92be396dd73933c828da0d5adc435718407dc127
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2022-12-31 17:48:56 -0300

    header-finish

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,23 @@
 import * as C from './styles'
+import logo from '../../imgs/logo.png';
+import menu from '../../imgs/menu.png';
 
 export const Header = () => {
     return (
         <C.Container> 
+            <div className='container'>
+                <div className='navbar'>
+                    <img className='logo' src={logo} alt=''></img>
+                    <nav>
+                        <ul>
+                        <li> <a href="W"> HOME </a></li>
+                        <li> <a href="W"> REGION </a></li>
+                        <li> <a href="W"> ABOUT </a></li>
+                        </ul>
+                    </nav>
+                    <img className='menu-icon' src={menu} alt=''></img>
+                </div>
+            </div>
         </C.Container>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,44 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
+*{
+    margin: 0;
+    padding: 0;
+    background-color: black;
+}
+.container{
+    width: 100%;
+    height: 100vh;
+    padding-left: 8%;
+    padding-right: 8%;
+    box-sizing: border-box;
+}
+.navbar{
+    height: 12%;
+    display: flex;
+    align-items: center;
+}
+.logo{
+    width: 50px;
+    cursor: pointer;
+}
+.menu-icon{
+    width: 30px;
+    cursor: pointer;
+    margin-left: 40px;
+}
+nav{
+    flex: 1;
+    text-align: right;
+}
+nav ul li{
+    list-style: none;
+    display: inline-block;
+    margin-left: 60px;
+}
+nav ul li a{ 
+    text-decoration: none;
+    color: #fff;
+    font-size: 13px;
+}
 `
