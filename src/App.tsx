f225637e67882d399a9b2e2e5a03697fdc0dc876
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2023-01-01 20:50:44 -0300

    component dash

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Dash } from './components/Dash';
 import { Header } from './components/Header'
 
 function App() {
   return (
     <>
       <Header/>
+      <Dash/>
     </>
   );
 }
---
+++ src/components/Dash/index.tsx
@@ -0,0 +1,9 @@
+import * as C from './styles';
+
+export const Dash = () => {
+    return (
+        <C.Container>
+        <h1>Dash</h1>
+        </C.Container>
+    )
+}
